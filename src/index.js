ba5bc793fc88c350291c2a300b3a0cf5327a1a29
ephetic <bpcleary@gmail.com>
2017-01-23 23:10:38 -0800

    closes #2 closes #3

--- src/index.js
+++ src/index.js
@@ -2,26 +2,25 @@ module.exports = function match(...patterns){
   return function(...args) {
     for (let [cond, _fn] of patterns) {
       const fn = typeof _fn == 'function' ? _fn : () => _fn
-
-      if ('function' == typeof cond && cond !== match) cond = cond(...args)
+      if (cond === match)                 cond = () => true
+      else if ('function' == typeof cond) cond = cond(...args)
       else if (Array.isArray(cond))       cond = eq(cond, args)
       else                                cond = eq([cond], args)
       if(cond) return fn(...args)
     }
   }
-  function eq(a,b) {
-    if (a === b || a === match) return true
-    if (!a || !b) return false
-    for (let key in Object.keys(a)) {
-      if (a[key] === match) continue
-      if ('object' == typeof a[key] && a[key].constructor == RegExp) return a[key].test(b[key])
-      if (!b.hasOwnProperty(key)) return false
-      if (a[key] === b[key]) continue
-      if ('string' == typeof b[key] && a[key] !== String) return false
-      if ('number' == typeof b[key] && a[key] !== Number) return false
-      if ('object' == typeof b[key] && a[key] === b.constructor) continue
-      return eq(a[key], b[key])
+  function eq(cond,val) {
+    if (cond === match)   return true
+    if (cond === val)     return true
+    if (cond === val.constructor) return true
+    if (cond instanceof RegExp)   return cond.test(val)
+    if (typeof cond !== 'object') return false
+
+    for (let key of Object.keys(cond)) {
+      if (!val.hasOwnProperty(key)) return false
+      if (!eq(cond[key], val[key])) return false
     }
     return true
   }
 }
+
--- tests/index.js
+++ tests/index.js
@@ -1,11 +1,12 @@
 const tape = require('tape')
 const match = require('../src')
+const _ = require('lodash')
 
 tape('Match on constants or predicate functions', t => {
   const isHi = match(['hi', true])
 
-  t.ok(isHi('hi'))
-  t.notOk(isHi('hello'))
+  t.ok(isHi('hi'), `Matched 'hi'`)
+  t.notOk(isHi('hello'), `Doesn't match 'hello`)
 
   const fact = match(
     [1, 1],
@@ -17,10 +18,9 @@ tape('Match on constants or predicate functions', t => {
     [match, n => fibo(n-1) + fibo(n-2)]
   )
 
-  for (var i = 1; i < 10; i++) {
-    t.equal(fibo(i), _fibo(i))
-    t.equal(fact(i), _fact(i))
-  }
+  const range = _.range(1,10)
+  t.ok(_(range).every(n => fibo(n) === _fibo(n)), `fibo works`)
+  t.ok(_(range).every(n => fact(n) === _fact(n)), `fact works`)
 
   t.end()
 
@@ -38,9 +38,9 @@ tape('Match on constants or predicate functions', t => {
 tape('Match with RegExp', t => {
   const isLikeHi = match([/.*hi.*/, s => `${s} is close enough`])
 
-  t.ok(isLikeHi('hi'))
-  t.ok(isLikeHi('shiny'))
-  t.notOk(isLikeHi('hello'))
+  t.ok(isLikeHi('hi'), `Matches 'hi'`)
+  t.ok(isLikeHi('shiny'), `Matching 'shiny'`)
+  t.notOk(isLikeHi('hello'), `Doesn't match 'hello'`)
   t.end()
 })
 
@@ -51,18 +51,18 @@ tape('Match on argument types, values, and don\'t-cares', t => {
     [match, (a,b) => `${b} is not a Number`]
   )
 
-  t.equal(secondIsNumber('asdf', 2), '2 is a Number')
-  t.equal(secondIsNumber(2, 'asdf'), 'asdf is not a Number')
+  t.equal(secondIsNumber('asdf', 2), '2 is a Number', `Second arg is Number`)
+  t.equal(secondIsNumber(2, 'asdf'), 'asdf is not a Number', `Second arg is not Number`)
   t.end()
 })
 
 tape('Match on given object keys', t => {
   const isDuckman = match([{name: 'Duckman', age:40}, true])
-  t.ok(isDuckman({name: 'Duckman', age:40}))
-  t.notOk(isDuckman({name: 'Duckman'}))
-  t.notOk(isDuckman({name: 'Birdperson', age:40}))
+  t.ok(isDuckman({name: 'Duckman', age:40}), `Object matches at given keys`)
+  t.notOk(isDuckman({name: 'Duckman'}), `Object is missing key`)
+  t.notOk(isDuckman({name: 'Birdperson', age:40}), `Object has incorrect value at key`)
 
   const isDuckPerson = match([{name: match, age: match}, true])
-  t.ok(isDuckperson({name: 'Rick', age: 55}))
+  t.ok(isDuckPerson({name: 'Rick', age: 55}), `Matches property types at keys`)
   t.end()
 })
